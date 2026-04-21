267315762fc1d74ca4ee6a3a67d67aedff115b9a
babarshah-91 <babar.shah@apexure.com>
2026-04-21 13:19:48 +0530

    final delpoyyy

--- apexure QA/index.ts
+++ apexure QA/index.ts
@@ -96,7 +96,7 @@ app.use((req, res, next) => {
   }
 
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(port, "localhost", () => {
+  httpServer.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
