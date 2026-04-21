c30fa49a4fcd7a4d400ff4d24633f690051f35c5
babarshah-91 <babar.shah@apexure.com>
2026-04-21 15:58:55 +0530

    add nixpacks build phase to generate

--- apexure QA/index.ts
+++ apexure QA/index.ts
@@ -96,7 +96,7 @@ app.use((req, res, next) => {
   }
 
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(port, "0.0.0.0", () => {
+  httpServer.listen(port, "::", () => {
     log(`serving on port ${port}`);
   });
 })();
