203ef5dafb1527caec9c2a8b6e491970c92e5831
babarshah-91 <babar.shah@apexure.com>
2026-05-04 15:08:28 +0530

    fix: add CORS support for figma plugin and update error messages

--- apexure QA/index.ts
+++ apexure QA/index.ts
@@ -1,4 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
@@ -36,6 +37,7 @@ app.use(
 );
 
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
+app.use(cors());
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
